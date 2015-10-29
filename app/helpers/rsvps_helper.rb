7ce1b229df8f88dc3a91daca53f4bb246350ae1c
Arya Iranpour <arya.iranpour@gmail.com>
2015-10-29 13:35:09 -0400

    Put back full nationbuilder access

--- app/helpers/rsvps_helper.rb
+++ app/helpers/rsvps_helper.rb
@@ -2,31 +2,31 @@ module RsvpsHelper
 
   def send_rsvp_to_nationbuilder(rsvp, person)
 
-    return {status: true, id: 10000000}
+    # return {status: true, id: 10000000}
 
-    # rsvpObject = rsvp.to_rsvp_object(person)
-    # if rsvpObject["rsvp"].has_key?("id")
-    #   begin
-    #     checkInResponse = token.put("/api/v1/sites/#{session[:current_site]}/pages/events/#{@current_event.eventNBID}/rsvps/#{rsvp.rsvpNBID}", :headers => standard_headers, :body => rsvpObject.to_json)
-    #     checked_in = JSON.parse(checkInResponse.body)["rsvp"]
-    #   rescue => ex
-    #     return {status: false, error: ex}
-    #   else
-    #     return {status: true, id: checked_in["id"].to_i }
-    #   end
+    rsvpObject = rsvp.to_rsvp_object(person)
+    if rsvpObject["rsvp"].has_key?("id")
+      begin
+        checkInResponse = token.put("/api/v1/sites/#{session[:current_site]}/pages/events/#{@current_event.eventNBID}/rsvps/#{rsvp.rsvpNBID}", :headers => standard_headers, :body => rsvpObject.to_json)
+        checked_in = JSON.parse(checkInResponse.body)["rsvp"]
+      rescue => ex
+        return {status: false, error: ex}
+      else
+        return {status: true, id: checked_in["id"].to_i }
+      end
 
-    # else
+    else
 
-    #   begin
-    #     checkInResponse = token.post("/api/v1/sites/#{session[:current_site]}/pages/events/#{@current_event.eventNBID}/rsvps/", :headers => standard_headers, :body => rsvpObject.to_json)
-    #   rescue => ex
-    #     return { status: false, error: ex }
-    #   else
-    #     checked_in = JSON.parse(checkInResponse.body)["rsvp"]
-    #     return {status: true, id: checked_in["id"].to_i }
-    #   end
+      begin
+        checkInResponse = token.post("/api/v1/sites/#{session[:current_site]}/pages/events/#{@current_event.eventNBID}/rsvps/", :headers => standard_headers, :body => rsvpObject.to_json)
+      rescue => ex
+        return { status: false, error: ex }
+      else
+        checked_in = JSON.parse(checkInResponse.body)["rsvp"]
+        return {status: true, id: checked_in["id"].to_i }
+      end
 
-    # end
+    end
 
   end
 
