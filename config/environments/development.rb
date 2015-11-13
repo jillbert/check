eed1740a8736e7d14c54bbb6efde528d60620eac
Arya Iranpour <arya.iranpour@gmail.com>
2015-11-13 12:46:20 -0500

    Prepped for production for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'gmail.com',
-    :user_name            => 'arya@cstreet.ca',
-    :password             => 'oreoboy86',
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASS"],
     :authentication       => 'plain',
     :enable_starttls_auto => true
     
