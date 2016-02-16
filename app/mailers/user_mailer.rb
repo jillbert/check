bf6fe94704f7ba9d825232af280dfe8af8696cc7
Arya Iranpour <arya.iranpour@gmail.com>
2016-02-16 15:51:41 -0500

    Added http://www.checkbycstreet.com/

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
   #
   def activation_needed_email(user)
     @user = user
-    @url  = "http://cstreetcheck.herokuapp.com/users/#{user.activation_token}/activate"
+    @url  = "http://www.checkbycstreet.com/users/#{user.activation_token}/activate"
     mail(:to => user.email,
          :subject => "Welcome to Check! Please activate your account")
   end
@@ -20,14 +20,14 @@ class UserMailer < ActionMailer::Base
   #
   def activation_success_email(user)
     @user = user
-    @url  = "http://cstreetcheck.herokuapp.com/login"
+    @url  = "http://www.checkbycstreet.com/login"
     mail(:to => user.email,
          :subject => "Your Check account is now activated")
   end
 
   def reset_password_email(user)
     @user = User.find user.id
-    @url  = "http://cstreetcheck.herokuapp.com/password_resets/#{@user.reset_password_token}/edit"
+    @url  = "http://www.checkbycstreet.com/password_resets/#{@user.reset_password_token}/edit"
     mail(:to => user.email,
          :subject => "Your password has been reset")
   end
