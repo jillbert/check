de9a829bc38f0f103919c17e56c527b77ee2c89a
Arya Iranpour <arya.iranpour@gmail.com>
2016-05-04 13:42:23 -0400

    Changed login credentials stuff

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,23 +14,22 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      nation = Nation.where(user_id: @user.id).first
-      credential = Credential.find_by nation_id: nation.id
+      if @user.active
+        nation = Nation.where(user_id: @user.id).first
+        credential = Credential.find_by nation_id: nation.id
 
-      set_current_nation(nation.id)
+        set_current_nation(nation.id)
 
-      if(credential)
-        set_current_credential(credential.id)
-      end
-
-      check_credential
-      if @credential && @user.active
-        redirect_back_or_to(choose_site_path, notice: 'Login successful')
+        if(credential)
+          set_current_credential(credential.id)
+          redirect_back_or_to(choose_site_path, notice: 'Login successful')
+        else
+          redirect_to authorize_path(nation_id: current_user.nation.id)
+        end
       else
         logout
         destroy_currents
-        flash.now[:alert] = "Sorry, but your account is suspended. If you think this is an error, please contact check@cstreet.ca"
-        render action: 'new'
+        redirect_to(login_path, notice: "Sorry your account has been suspended. If you think is in error, please contact check@cstreet.ca.")
       end
 
     else
