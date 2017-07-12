c23c470fd3b6c24011340eed727ebf39aa2809ee
Alex Flint <alex@cstreet.ca>
2017-07-12 14:03:54 -0400

    changed activatino URL in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
   #
   def activation_needed_email(user)
     @user = user
-    @url  = "http://www.checkbycstreet.com/users/#{user.activation_token}/activate"
+    @url  = "https://checkv2.herokuapp.com/users/#{user.activation_token}/activate"
     mail(:to => user.email,
          :subject => "Welcome to Check! Please activate your account")
   end
@@ -20,14 +20,14 @@ class UserMailer < ActionMailer::Base
   #
   def activation_success_email(user)
     @user = user
-    @url  = "http://www.checkbycstreet.com/login"
+    @url  = "https://checkv2.herokuapp.com/login"
     mail(:to => user.email,
          :subject => "Your Check account is now activated")
   end
 
   def reset_password_email(user)
     @user = User.find user.id
-    @url  = "http://www.checkbycstreet.com/password_resets/#{@user.reset_password_token}/edit"
+    @url  = "https://checkv2.herokuapp.com/password_resets/#{@user.reset_password_token}/edit"
     mail(:to => user.email,
          :subject => "Your password has been reset")
   end
