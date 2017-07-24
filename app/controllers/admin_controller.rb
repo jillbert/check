609b7f1af41b15b44c0de6d28341dc3f956c1111
Alex Flint <alex@cstreet.ca>
2017-07-24 09:51:06 -0400

    fixed is_admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,12 @@ class AdminController < ApplicationController
   before_filter :is_admin
 
   def index
-    redirect_to rsvps_path unless ENV['ADMIN_ID'].include? current_user.id.to_s 
     @users = User.all.order('created_at DESC')
   end
 
   private
 
   def is_admin
-    redirect_to(:root, flash: { error: "Sorry, you don't have access to this information" }) if current_user.id != ENV['ADMIN_ID'].to_i
+    redirect_to(:root, flash: { notice: "Page not found" }) unless ENV['ADMIN_ID'].include? current_user.id.to_s
   end
 end
