701ccad6ebea0eee8825295d4ac4ab927e46069d
Alex Flint <alex@cstreet.ca>
2017-07-24 09:47:01 -0400

    changed admin check

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_filter :is_admin
 
   def index
-    redirect_to rsvps_path unless current_user.id == ENV['ADMIN_ID'].to_i
+    redirect_to rsvps_path unless ENV['ADMIN_ID'].include? current_user.id.to_s 
     @users = User.all.order('created_at DESC')
   end
 
