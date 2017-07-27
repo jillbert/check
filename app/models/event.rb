5bfaf7ac4638ea168e83ae87ec6ee355515d0c32
Alex Flint <alex@cstreet.ca>
2017-07-27 10:43:59 -0400

    fixed up showCard() function.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,17 +4,17 @@ class Event < ActiveRecord::Base
 
   def self.import(nb, session_nation)
     event = self.find_or_create_by(
-      nation_id: session_nation, 
-      eventNBID: nb['id'].to_i, 
+      nation_id: session_nation,
+      eventNBID: nb['id'].to_i,
     )
 
     event.update(
-      name: nb['name'], 
-      start_time: nb['start_time'].to_datetime, 
+      name: nb['name'],
+      start_time: nb['start_time'].to_datetime,
       end_time: nb['end_time'].to_datetime,
-      time_zone: nb['time_zone'] 
+      time_zone: nb['time_zone']
     )
-    
+
     return event
   end
 
