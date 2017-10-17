4b66c5e4a0d908ddacd5e6c16ef2a72890617892
Alex Flint <alex@cstreet.ca>
2017-10-17 15:21:50 -0400

    finished building importnation

--- app/workers/import_nation.rb
+++ app/workers/import_nation.rb
@@ -1,7 +1,7 @@
 class ImportNation
   @queue = :import_nation
 
-  def self.perform(nation_id, site_slug)
+  def self.perform(nation_id)
     nation = Nation.find(nation_id.to_i)
     nb_client = NationBuilder::Client.new(nation.name,
                                           nation.credentials.first.token, retries: 8)
@@ -9,8 +9,9 @@ class ImportNation
     sites["results"].each do |site|
       slug = site["slug"]
       events = nb_client.call(:events, :index, site_slug: slug, limit: 1000)
-      events.each do |event|
-        event = Event.find_or_create_by(nation_id: nation.id, eventNBID: event["id"])
+      events['results'].each do |e|
+        event = Event.import(e, nation.id)
+        Rsvp.sync(event, slug)
       end
     end
   end
